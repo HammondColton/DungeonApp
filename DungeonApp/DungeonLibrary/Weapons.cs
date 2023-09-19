69dfa4474ba7904e65ca6166411c6127210627a6
Colton Hammond <HammondColton@outlook.com>
2023-09-19 16:08:58 -0500

    added classes weapon and character

---
+++ DungeonApp/DungeonLibrary/Weapons.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonLibrary
+{
+    internal class Weapons
+    {
+        private int _minDamage;
+        private int _maxDamage;
+        private string _name;
+        private int _bonusHitChance;
+        private bool _isTwoHanded;
+
+        public int MinDamage
+        {
+            get { return _minDamage; }
+            set { _minDamage = value; }
+        }
+        public int MaxDamage
+        {
+            get { return _maxDamage; }
+            set { _maxDamage = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public int BonusHitChance
+        {
+            get { return _bonusHitChance; }
+            set { _bonusHitChance = value; }
+        }
+        public bool IsTwoHanded
+        {
+            get { return _isTwoHanded; }
+            set { _isTwoHanded = value; }
+        }
+
+
+
+
+    }
+}
